746cedf2bae8a85d5f705550cfc6e0ef6b60f67c
Cristian Csordas <cristicsordas@gmail.com>
2023-06-22 16:42:24 +0200

    Added log info

--- server/src/drivers/PCA9685_KernelSpace/I2CPeripheral.cpp
+++ server/src/drivers/PCA9685_KernelSpace/I2CPeripheral.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <iostream>
+
 extern "C"
 {
 #include <linux/i2c.h>
@@ -19,12 +21,14 @@ I2CPeripheral::I2CPeripheral(const std::string &device)
 
 I2CPeripheral::~I2CPeripheral()
 {
+  std::cout << "close device" << std::endl;
   fclose(fp);
 }
 
 void I2CPeripheral::WriteRegisterByte(const uint8_t register_address, const uint8_t value)
 {
   uint8_t buf[2] = {register_address, value}; 
+  std::cout << "i2c write addr " << register_address << " value " << value << std::endl;
   fwrite((char*)buf, 1, sizeof(buf), fp);
 }
 
@@ -43,8 +47,10 @@ uint8_t I2CPeripheral::ReadRegisterByte(const uint8_t register_address)
 void I2CPeripheral::OpenBus(const std::string &device)
 {
   fp = fopen(device.c_str(), "a+");
+  std::cout << "open device " << device << std::endl;
   if(fp == nullptr)
   {
+    std::cout << "open device error";
     throw std::system_error(errno, std::system_category(), "Could not open pca i2c communication");
   }
 }
